ff3175f1b5c959a2f88d6cb45bfdd70faf359998
Yasiru Munasinghe <yasiruchathu55@gmail.com>
2023-09-01 18:31:11 +0530

    app-routing file

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  username: string = '';
+  password: string = '';
 
+  onSubmit() {
+    // You can add authentication logic here, e.g., send a request to a backend service.
+    // For simplicity, we'll just log the username and password for now.
+    console.log('Username:', this.username);
+    console.log('Password:', this.password);
+  }
 }
