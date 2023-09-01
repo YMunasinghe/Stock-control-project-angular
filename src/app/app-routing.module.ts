db489435d319465754a0c13f8d84880b0e5851a8
Yasiru Munasinghe <yasiruchathu55@gmail.com>
2023-09-02 03:31:00 +0530

    user image

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,16 @@ import { CommonModule } from '@angular/common';
 import {RouterModule, Routes} from "@angular/router";
 import {LoginComponent} from "./components/login/login.component";
 import {TopBarComponent} from "./components/top-bar/top-bar.component";
+import {SideNavBarComponent} from "./components/side-nav-bar/side-nav-bar.component";
+import {LoggedInTopBarComponent} from "./components/logged-in-top-bar/logged-in-top-bar.component";
+import {LoggedInComponent} from "./components/logged-in/logged-in.component";
 
 const routes: Routes = [
-  { path: 'login', component: LoginComponent },
   { path: '', component: TopBarComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'sideNavBar', component: SideNavBarComponent },
+  { path: 'loggedInTopBar', component: LoggedInTopBarComponent },
+  { path: 'loggedIn', component: LoggedInComponent },
 ]
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
