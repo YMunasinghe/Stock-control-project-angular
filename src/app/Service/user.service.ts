784015335c2dfcd8b6901fcb916ab226f4c83f2a
Yasiru Munasinghe <yasiruchathu55@gmail.com>
2023-09-06 15:24:17 +0530

    successfully logged

--- src/app/Service/user.service.ts
+++ src/app/Service/user.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import {Observable} from "rxjs";
 import {HttpClient} from "@angular/common/http";
+import {User} from "../../user";
 
 @Injectable({
   providedIn: 'root'
@@ -8,9 +9,16 @@ import {HttpClient} from "@angular/common/http";
 export class UserService {
 
   private apiUrl='http://localhost:8080';
+  private baseUrl='http://localhost:8080/user/login';
   constructor(private httpClient:HttpClient) { }
 
   getUsers():Observable<any>{
     return  this.httpClient.get<any>('http://localhost:8080/user/getAllUsers')
+  }
+
+  loginUser(user:User):Observable<object>{
+    // return this.httpClient.post(`${this.apiUrl}`+'/user/login',user);
+    console.log(user);
+    return this.httpClient.post(`${this.baseUrl}`,user);
   }
 }
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {User} from "../../../user";
+import {UserService} from "../../Service/user.service";
 
 @Component({
   selector: 'app-login',
@@ -6,13 +8,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
-  username: string = '';
-  password: string = '';
 
-  onSubmit() {
+  user:User=new User();
+
+  constructor(private userService:UserService) {
+
+  }
+
+  // username: string = '';
+  // password: string = '';
+
+  userLogin() {
     // You can add authentication logic here, e.g., send a request to a backend service.
     // For simplicity, we'll just log the username and password for now.
-    console.log('Username:', this.username);
-    console.log('Password:', this.password);
+    // console.log('Username:', this.username);
+    // console.log('Password:', this.password);
+
+    this.userService.loginUser(this.user).subscribe(data=>{
+      alert("Login Successful");
+    },error =>alert("Sorry, Invalid Credentials"));
   }
 }
