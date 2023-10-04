65c79a0c35b93bfe562f3d77fff38b6250fe4c01
Yasiru Munasinghe <yasiruchathu55@gmail.com>
2023-10-05 00:06:21 +0530

    updated

--- src/app/components/customer-master/customer-master.component.ts
+++ src/app/components/customer-master/customer-master.component.ts
@@ -12,6 +12,10 @@ export class CustomerMasterComponent {
   @ViewChild('customer_table', {static:false}) el!:ElementRef;
 
   customers: any = [];
+  customersSecond: any = [];
+
+  customerSearchTerm: string = '';
+  searchResults: string[] = [];
 
   constructor(private customerService : CustomerService) {}
 
@@ -22,7 +26,34 @@ export class CustomerMasterComponent {
     this.customerService.getCustomers().subscribe((customer)=>{
       console.log("users",customer);
       this.customers=customer;
+      this.searchCustomer();
     })
+
+  }
+
+  searchCustomer(): void {
+    // Implement your search logic here
+    // This could involve making HTTP requests to a backend service or filtering data locally
+    // For simplicity, we'll just split the search term by space and return matching results
+
+    /**
+     * have to sort by name part to do
+     */
+    if (this.customerSearchTerm.trim() === '') {
+      this.customersSecond = this.customers;
+      return;
+    }else {
+      this.customersSecond = [];
+      return;
+    }
+
+    // const searchTerms = this.searchTerm.split(' ');
+    // // Mock data for demonstration purposes
+    // // const data = ['Result 1', 'Result 2', 'Result 3', 'Result 4', 'Result 5'];
+
+    // this.searchResults = this.customers.filter(result => {
+    //   return searchTerms.every(term => result.toLowerCase().includes(term.toLowerCase()));
+    // });
   }
 
   // const doc = new jsPDF();
--- src/app/components/search-item/search-item.component.ts
+++ src/app/components/search-item/search-item.component.ts
@@ -12,7 +12,7 @@ export class SearchItemComponent {
   constructor() { }
 
   search(): void {
-    // Implement your search logic here
+    // search logic
     // This could involve making HTTP requests to a backend service or filtering data locally
     // For simplicity, we'll just split the search term by space and return matching results
 
@@ -21,7 +21,7 @@ export class SearchItemComponent {
       return;
     }
 
-    const searchTerms = this.searchTerm.split(' ');
+    const searchTerms = this.searchTerm.split('');
     // Mock data for demonstration purposes
     const data = ['Result 1', 'Result 2', 'Result 3', 'Result 4', 'Result 5'];
 
@@ -29,5 +29,4 @@ export class SearchItemComponent {
       return searchTerms.every(term => result.toLowerCase().includes(term.toLowerCase()));
     });
   }
-
 }
